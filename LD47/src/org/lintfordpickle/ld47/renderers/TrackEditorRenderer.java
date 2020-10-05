5d9dbc410e4746b4267af7dd3b5378efb5c08ae4
John Hampson <jonnohampson@gmail.com>
2020-10-05 15:49:04 +0200

    TrackEditor renders tracks (and not just the nodes)

--- LD47/src/org/lintfordpickle/ld47/renderers/TrackEditorRenderer.java
+++ LD47/src/org/lintfordpickle/ld47/renderers/TrackEditorRenderer.java
@@ -5,11 +5,19 @@ import java.util.ArrayList;
 import org.jbox2d.common.Vec2;
 import org.lintfordpickle.ld47.controllers.TrackEditorController;
 import org.lintfordpickle.ld47.data.track.Edge;
+import org.lintfordpickle.ld47.data.track.Node;
+import org.lintfordpickle.ld47.data.track.Track;
+import org.lintfordpickle.ld47.graphics.TrackBatchPT;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 
 import net.lintford.library.core.LintfordCore;
+import net.lintford.library.core.ResourceManager;
 import net.lintford.library.core.debug.Debug;
+import net.lintford.library.core.graphics.sprites.spritesheet.SpriteSheetDefinition;
+import net.lintford.library.core.graphics.textures.Texture;
+import net.lintford.library.core.graphics.textures.texturebatch.TextureBatchPCT;
+import net.lintford.library.core.maths.Vector2f;
 import net.lintford.library.renderers.BaseRenderer;
 import net.lintford.library.renderers.RendererManager;
 
@@ -25,6 +33,9 @@ public class TrackEditorRenderer extends BaseRenderer {
 	// Variables
 	// ---------------------------------------------
 
+	private Texture mTrackTexture;
+	private SpriteSheetDefinition mWorldSpriteSheet; // signals
+	private TrackBatchPT mTrackBatchPCT;
 	private TrackEditorController mTrackEditorController;
 
 	// ---------------------------------------------
@@ -43,6 +54,8 @@ public class TrackEditorRenderer extends BaseRenderer {
 	public TrackEditorRenderer(RendererManager pRendererManager, int pEntityGroupID) {
 		super(pRendererManager, RENDERER_NAME, pEntityGroupID);
 
+		mTrackBatchPCT = new TrackBatchPT();
+
 	}
 
 	// ---------------------------------------------
@@ -56,6 +69,26 @@ public class TrackEditorRenderer extends BaseRenderer {
 	}
 
 	@Override
+	public void loadGLContent(ResourceManager pResourceManager) {
+		super.loadGLContent(pResourceManager);
+
+		mTrackTexture = pResourceManager.textureManager().loadTexture("TEXTURE_TRACK", "res/textures/textureTrack.png", GL11.GL_NEAREST, entityGroupID());
+		mWorldSpriteSheet = pResourceManager.spriteSheetManager().getSpriteSheet("SPRITESHEET_WORLD", entityGroupID());
+
+		mTrackBatchPCT.loadGLContent(pResourceManager);
+
+	}
+
+	@Override
+	public void unloadGLContent() {
+		super.unloadGLContent();
+
+		mTrackBatchPCT.unloadGLContent();
+		mWorldSpriteSheet = null;
+
+	}
+
+	@Override
 	public void draw(LintfordCore pCore) {
 		if (!mTrackEditorController.isinitialized())
 			return;
@@ -63,6 +96,9 @@ public class TrackEditorRenderer extends BaseRenderer {
 		// Draw world origin
 		Debug.debugManager().drawers().drawPointImmediate(pCore.gameCamera(), 0.f, 0.f);
 
+		final var lTrack = mTrackEditorController.track();
+		drawTrack(pCore, lTrack);
+		
 		drawEdges(pCore);
 		drawNodes(pCore);
 
@@ -259,4 +295,85 @@ public class TrackEditorRenderer extends BaseRenderer {
 
 	}
 
+	// ------------------------
+	
+	private void drawTrack(LintfordCore pCore, Track pTrack) {
+		final var lEdgeList = pTrack.edges;
+
+		final var lSignalBatch = mRendererManager.uiTextureBatch();
+
+		lSignalBatch.begin(pCore.gameCamera());
+		mTrackBatchPCT.begin(pCore.gameCamera());
+
+		final var lEdgeCount = lEdgeList.size();
+		for (int i = 0; i < lEdgeCount; i++) {
+			final var lEdge = lEdgeList.get(i);
+
+			final var lNodeA = pTrack.getNodeByUid(lEdge.nodeAUid);
+			final var lNodeB = pTrack.getNodeByUid(lEdge.nodeBUid);
+
+			drawTrackSegment(pCore, mTrackBatchPCT, lNodeA, lNodeB);
+
+			if (lEdge.signalNode != null && lEdge.signalNode.isSignalActive) {
+				drawSignalBox(pCore, lSignalBatch, pTrack, lEdge, lNodeA);
+
+			}
+
+		}
+
+		mTrackBatchPCT.end();
+		lSignalBatch.end();
+
+	}
+
+	private void drawTrackSegment(LintfordCore pCore, TrackBatchPT pTextureBatch, Node pTrackNodeA, Node pTrackNodeB) {
+		final var lSegmentLength = Vector2f.distance(pTrackNodeA.worldPositionX, pTrackNodeA.worldPositionY, pTrackNodeB.worldPositionX, pTrackNodeB.worldPositionY);
+
+		final var lRotX = -0.f;
+		final var lRotY = -lSegmentLength * .5f;// lSegmentLength;
+		final var lSegmentAngle = (float) Math.atan2(pTrackNodeB.worldPositionY - pTrackNodeA.worldPositionY, pTrackNodeB.worldPositionX - pTrackNodeA.worldPositionX) + (float) Math.toRadians(-90);
+
+		// Variable seg length
+		final float lPosX = pTrackNodeA.worldPositionX;
+		final float lPosY = pTrackNodeA.worldPositionY;
+		final float lPosW = 8.f;
+		final float lPosH = lSegmentLength;
+
+		final float lSrcX = 0;
+		final float lSrcY = 0;
+		final float lSrcW = 16.f;
+		final float lSrcH = 32.f;
+
+		// WOPRLD_TEXTURE
+		pTextureBatch.drawAroundCenter(mTrackTexture, lSrcX, lSrcY, lSrcW, lSrcH, lPosX, lPosY, lPosW, lPosH, -0.1f, lSegmentAngle, lRotX, lRotY, lSegmentLength);
+
+	}
+
+	private void drawSignalBox(LintfordCore pCore, TextureBatchPCT pTextureBatch, Track pTrack, Edge pActiveEdge, Node pTrackNode) {
+		final var lIsLeftSignalActive = pActiveEdge.signalNode.leftEnabled;
+		final var lActiveEdgeUid = lIsLeftSignalActive ? pActiveEdge.signalNode.leftEdgeUid : pActiveEdge.signalNode.rightEdgeUid;
+		final var lActiveEdge = pTrack.getEdgeByUid(lActiveEdgeUid);
+
+		final int pCommonNodeUid = Edge.getCommonNodeUid(pActiveEdge, lActiveEdge);
+
+		final var lActiveNode = pTrack.getNodeByUid(pCommonNodeUid);
+		final var lOtherNodeUid = lActiveEdge.getOtherNodeUid(lActiveNode.poolUid);
+		final var lOtherNode = pTrack.getNodeByUid(lOtherNodeUid);
+		final float lVectorX = lOtherNode.worldPositionX - lActiveNode.worldPositionX;
+		final float lVectorY = lOtherNode.worldPositionY - lActiveNode.worldPositionY;
+
+		Vec2 ll = new Vec2(lVectorX, lVectorY);
+		ll.normalize();
+
+		final var lWorldTexture = mWorldSpriteSheet.texture();
+		final var lSignalBounds = lIsLeftSignalActive ? mWorldSpriteSheet.getSpriteFrame("TEXTURESIGNALLEFT") : mWorldSpriteSheet.getSpriteFrame("TEXTURESIGNALRIGHT");
+		pTextureBatch.draw(lWorldTexture, lSignalBounds, lActiveNode.worldPositionX - 8.f, lActiveNode.worldPositionY - 16.f, 16.f, 16.f, -0.1f, 1, 1, 1, 1);
+
+		// Debug.debugManager().drawers().drawCircleImmediate(pCore.gameCamera(), lActiveNode.worldPositionX + ll.x * 20.f, lActiveNode.worldPositionY + ll.y * 20.f, 4.f);
+
+	}
+
+	
+	
+	
 }
